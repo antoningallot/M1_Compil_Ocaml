007f65b6eb604885a52bd93628431ba21e5d92e3
Nicolas Devatine <nico.devatine@gmail.com>
2018-09-15 14:48:28 +0200

    Quelques modifs SourceTypeChecker.ml

--- SourceTypeChecker.ml
+++ SourceTypeChecker.ml
@@ -7,27 +7,23 @@ let type_literal = function
   | Int _ -> TypInt
   | Bool _ -> TypBool
 
-let check_type context i e ty =
+let rec check_type context pos e ty =
   let t = type_expression context e in
   if t = ty
   then ()
-  else raise (Type_error(ty, t, i.i_pos))
-    
-let rec type_expression context e = match e.expr with
+  else raise (Type_error(ty, t, pos))
+and type_expression context e = match e.expr with
   | Literal a ->  type_literal a
   | Location(Identifier(Id name)) -> Symb_Tbl.find name context.identifier_types
-  | UnaryOp (a, b) ->
-     match a with
-     | Minus -> check_type context e b TypInt
-     | Not -> check_type context e b TypBool
-  |
-       
-  | BinaryOp (a, b, c) -> 
-      
+  | UnaryOp (Minus, b) -> check_type context e.e_pos b TypInt; TypInt
+  | UnaryOp (Not, b) ->  check_type context e.e_pos b TypBool; TypBool
+  (* | BinaryOp (a, b, c) -> *)
+    
 let rec typecheck_instruction context i = match i.instr with
-  | Print e -> check_type context i e TypInt
+  | Print e -> check_type context i.i_pos e TypInt
   | Nop -> ()
-     
+   
+   
 let extract_context p =
   { identifier_types = p.globals; }
     
